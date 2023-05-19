d3030f7cd5a631204db79bfb8cb10899ab1ad48f
Fitry <mohdfitri2324@gmail.com>
2023-05-20 03:25:30 +0800

    change font and color

--- main.js
+++ main.js
@@ -18,17 +18,13 @@ function clickElement(){
             navBar.style.transition = 'transition: all 0.2s ease-in-out';
         }
     });
-    // document.getElementById("navbar").addEventListener("click", ()=> {
-    //     // alert("Clicked");
-    
-
-    //     // navBar.classList.toggle('nav-toggle');
-    
-    //     if(navBar.style.left == '0%'){
-    //         navBar.style.left = '-100%';
-    //         navBar.style.transition = 'transition: all 0.2s ease-in-out';
-    //     }
-    // });
+    document.getElementById("navbar").addEventListener("click", ()=> {
+        // var mediaQuery = window.matchMedia("(max-width: 968px)");
+        if(navBar.style.left == '0%'){
+            navBar.style.left = '-100%';
+            navBar.style.transition = 'transition: all 0.2s ease-in-out';
+        }
+    });
 }
 function toggleNavbar(){
     var hamburgers = document.getElementById("hamburger");
